31111cb4b4e2704ed9942e84d3aac19bbae18ffe
zefangracio <48423324+zefangracio@users.noreply.github.com>
2020-02-23 11:32:04 +0700

    Add files via upload

---
+++ ExceptionError/src/exceptionerror/CountLetters.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptionerror;
+
+/**
+ *
+ * @author zefan
+ */
+import java.util.Scanner;
+public class CountLetters
+{
+ public static void main(String[] args)
+ {
+ int[] counts = new int[26];
+ Scanner scan = new Scanner(System.in);
+int i =0;
+ //get word from user
+ System.out.print("Enter a single word (letters only, please): ");
+ String word = scan.nextLine();
+ //convert to all upper case
+ word = word.toUpperCase();
+ //count frequency of each letter in string
+ try{
+ for ( i=0; i < word.length(); i++)
+ counts[word.charAt(i)-'A']++;
+ }
+ catch(ArrayIndexOutOfBoundsException ex){
+     System.out.print(word.charAt(i) + " Not a Letter");
+ }
+ //print frequencies
+ System.out.println();
+ for ( i=0; i < counts.length; i++)
+ if (counts [i] != 0)
+ System.out.println((char)(i +'A') + ": " + counts[i]);
+ } 
+}
